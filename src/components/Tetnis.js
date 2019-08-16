c0ef4b49884b8e45264ef42b3c1c6c90f1185e27
Rusty Hoppins <rustiphyde@gmail.com>
2019-08-16 02:15:05 -0500

    Initial Commit of Tetris clone with component scaffolding

---
+++ src/components/Tetnis.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+const Tetnis = () => {
+    return (
+        <div>
+            <Stage />
+            <aside>
+                <div>                    
+                <Display text="Score" />
+                <Display text="Rows" />
+                <Display text="Level" />
+                </div>
+                <StartButton /> 
+            </aside>
+        </div>
+    )
+}
+
+export default Tetnis;
