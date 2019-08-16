cd4722bf8b980606a105ee321a97f65cd1b90ae9
Rusty Hoppins <rustiphyde@gmail.com>
2019-08-16 02:57:23 -0500

    Create rustominos shapes and colors with random function and gameHelpers for the display

---
+++ src/rustominos.js
@@ -0,0 +1,38 @@
+export const RUSTOMINOS = {
+  0: { shape: [[0]], color: "0, 0, 0" },
+  I: {
+    shape: [[0, "I", 0, 0], [0, "I", 0, 0], [0, "I", 0, 0], [0, "I", 0, 0]],
+    color: "133, 32, 6"
+  },
+  J: {
+    shape: [[0, "J", 0], [0, "J", 0], ["J", "J", 0]],
+    color: "242, 127, 17"
+  },
+  L: {
+    shape: [[0, "L", 0], [0, "L", 0], [0, "L", "L"]],
+    color: "141, 79, 12"
+  },
+  O: {
+    shape: [["O", "O"], ["O", "O"]],
+    color: "179, 192, 125"
+  },
+  S: {
+    shape: [[0, "S", "S"], ["S", "S", 0], [0, 0, 0]],
+    color: "253, 220, 84"
+  },
+  T: {
+    shape: [[0, 0, 0], ["T", "T", "T"], [0, "T", 0]],
+    color: "246, 250, 235"
+  },
+  Z: {
+    shape: [["Z", "Z", 0], [0, "Z", "Z"], [0, 0, 0]],
+    color: "251, 171, 105"
+  }
+}
+
+export const randomRustomino = () => {
+    const rustominos = 'IJLOSTZ';
+    const randRustomino = 
+        rustominos[Math.floor(Math.random() * rustominos.length)];
+        return RUSTOMINOS[randRustomino];
+}
