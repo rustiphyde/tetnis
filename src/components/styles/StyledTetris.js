9995008bdf870d0b4f6c50ae366a2cd034fe7e97
rustiphyde <rustiphyde@gmail.com>
2020-02-05 23:52:49 -0600

    change element positions

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -12,7 +12,8 @@ export const StyledTetrisWrapper = styled.div`
 
 export const StyledTetris = styled.div`
   display: flex;
-  align-items: flex-start;
+  flex-direction: column;
+  align-items: center;
   padding: 40px;
   margin: 0 auto;
   aside {
