765dad022939e71e408883d80dc1aaee1a58fd99
rustiphyde <rustiphyde@gmail.com>
2020-02-05 23:13:59 -0600

    Adjust stage size

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -4,12 +4,12 @@ export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
     ${props => props.height},
-    calc(50vw / ${props => props.width})
+    calc(25vw / ${props => props.width})
   );
   grid-template-columns: repeat(${props => props.width}, 1fr);
   grid-gap: 1px;
   border: 6px groove #b7410e;
   width: 100%;
-  max-width: 50vw;
+  max-width: 25vw;
   background: #411705;
 `;
